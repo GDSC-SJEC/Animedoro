5149a45683bfd51d4181ac62344440fff55129ba
Avinash Muralidharan <87583508+avi7501@users.noreply.github.com>
2022-10-12 15:52:42 +0530

    Changed the background color for main activity

--- app/src/main/java/com/example/animedoro/MainActivity.kt
+++ app/src/main/java/com/example/animedoro/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.example.animedoro.ui.theme.AnimedoroTheme
+import com.example.animedoro.ui.theme.primary
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = primary
                 ) {
                     AnimeradoMainScreen()
                 }
